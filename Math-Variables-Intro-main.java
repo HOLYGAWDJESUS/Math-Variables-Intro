c35174a05214d0ba6b8c08102fbe8fa3232d26ca
HOLYGAWDJESUS <99631231+HOLYGAWDJESUS@users.noreply.github.com>
2025-08-19 10:48:01 -0700

    Add files via upload

---
+++ Math-Variables-Intro-main.java
@@ -0,0 +1,72 @@
+class Main {
+  public static void main(String[] args) {
+
+    //Use the // to create single line comments to either add info or to take out code for debugging
+
+    System.out.println("Hello world!");
+    System.out.println(5 + " green apples");
+
+
+    //We will be using System.out.println and System.out.print throughout the year. Try using both below to see what the difference is!
+
+
+
+
+
+
+    //ANSWER: System.out.println prints on the next line while System.out.println just prints on the same line as before.
+
+
+
+    //Throughout the year this year we will need to store information. For that we will be using VARIABLES!
+
+    //Java is an Object-Oriented programming language. All variables we use this year will either be OBJECTS or PRIMITIVES
+
+    //There are 8 primitives in Java: int, byte, short, long, float, double, boolean, char
+
+    //For AP we need to know: int, double, boolean
+
+    //List examples of the types below and give definition
+    //int (integer): An integer number
+    //double: A specified number with more infomation (decimals)
+    //boolean: A true or false data type.
+
+    //For now we are just going to work with primitive
+
+    //Create 3 variables of each of the above types (USE GOOD CODING PRACTICE WHEN CREATING THE VARIABLES
+
+    int integerVariable = 2;
+    double realVariavle = 2.5;
+    boolean truefalseVariable = true;
+
+    //MATH TIME!
+
+    //What are the math operators that we can use?
+    //+ - * / %
+
+    //Try doing some math operations with numbers. How can we check to see if the math worked?
+
+    //Create codes that will print the following:
+
+    //Odd integers from 1 to 100, inclusive of both
+
+    System.out.println("all odd integers less than 100");
+    for (int i = 1; i < 100; i += 2) {
+      System.out.println(i);
+    }
+
+    //All multiples of 3 from 1 to 100
+
+    System.out.println("all natural mutiples of 3 less than 100");
+    for (int i = 3; i < 100; i += 3) {
+      System.out.println(i);
+    }
+
+    //Starting at 1000, print on the same line (with a - [hypthen] between each) all of the numbers that end in 0 going down to 0
+
+    for (int i = 1000; i>0; i-=10){
+    System.out.print(i + "-");
+  }
+    System.out.print(0);
+  }
+}
